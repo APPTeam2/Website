cec2e658ae15715994877176186a219df707601f
a10a <antoine.guillot4@free.fr>
2016-06-16 11:17:30 +0200

    spécial adrien

--- assets/script/header.js
+++ assets/script/header.js
@@ -3,30 +3,63 @@
 
 
 function formulaire() {
-  $formulaire_login=$('#formulaire_co_js');
-  $formulaire_login.show();
+    $formulaire_login=$('#formulaire_co_js');
+    $formulaire_login.show();
 
 
-  $login=$('ol');
-  $login.hide();
+    $login=$('ol');
+    $login.hide();
 
-  $nav=$('#texte_titre');
-  $nav.after("");
+    $nav=$('#texte_titre');
+    $nav.after("");
 
 
 
-  $formulaire_login.on
-  ( 'click', function () 
-    {
-      $login.show();
-    }
-  );
+    $formulaire_login.on
+    ( 'click', function () 
+        {
+            $login.show();
+        }
+    );
 
 
 }
 
 
+function redirection() 
+{
+  var url = window.location.href.split( '/' );
+  var len = url.length;
+  var url_f="";
+
+  var i = 0;
+  for (; i < len-2; i++) {
+      url_f=url_f+(url[i]);
+      url_f=url_f+('/');
+  }
+  document.location.href=url_f; 
+
+}
+
+function option()
+{
+    var url = window.location.href.split( '/' );
+    var len = url.length;
+    var url_f="";
+    var i = 0;
+    for (; i < len-2; i++) 
+    {
+      url_f=url_f+(url[i]);
+      url_f=url_f+('/');
+    }
+
+    $formulaire_login=$('#formulaire_co_js');
+    $formulaire_login.after("<li>"+'<img src="'+url_f+'assets/img/logo_titre.jpg" id="logo_titre" alt="Logo titre"/>'+"</li>");
+
+}
 
 
+option();
 
 formulaire();
+
