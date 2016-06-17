bd9310db134eeef7b3d0a6a3b2fd93ec714299ad
a10a <antoine.guillot4@free.fr>
2016-06-17 14:34:39 +0200

    maj injection sql

--- application/models/Confirmation_mail.php
+++ application/models/Confirmation_mail.php
@@ -14,7 +14,7 @@ Class Confirmation_mail extends CI_Model
 
       if($code == $code_confirmation)
       {
-         $requete = "update utilisateur SET actif = '1' WHERE idUser='".$id."'";
+         $requete = "update utilisateur SET actif = '1' WHERE idUser=".$this->db->escape($id).";";
          $result = $this->db->query($requete);
 
          return true;
@@ -28,7 +28,7 @@ Class Confirmation_mail extends CI_Model
 
    function non_confirmation($mail)
    {
-      $requete = "select actif FROM utilisateur WHERE mail='".$mail."'";
+      $requete = "select actif FROM utilisateur WHERE mail=".$this->db->escape($mail).";";
       $result = $this->db->query($requete);
 
       $result= $result->row(0);
@@ -37,7 +37,7 @@ Class Confirmation_mail extends CI_Model
 
       if ($result==1)
       {
-         $requete = "update utilisateur SET actif = '0' WHERE mail='".$mail."'";
+         $requete = "update utilisateur SET actif = '0' WHERE mail=".$this->db->escape($mail).";";
          $this->db->query($requete);
          return(TRUE);
       }
