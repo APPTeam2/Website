aab5290cd486d8e3455692498ce48ebc162fa5aa
El0him <apironneau.ir2018@esaip.org>
2016-06-19 17:00:17 +0200

    Tentative (infructueuse) d'envoyer un mail ~

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -25,39 +25,42 @@ class Contact extends CI_Controller
         }
     
     
-          //v 
-        function sendform()
+        function sendMail() 
         {
+            $config = Array
+                (
+                'protocol' => 'smtp',
+                'smtp_host' => 'ssl://smtp.googlemail.com',
+                'smtp_port' => 465,
+                'smtp_user' => 'finlandblogproject@gmail.com', // change it to yours
+                'smtp_pass' => 'finland2016', // change it to yours
+                'mailtype' => 'html',
+                'charset' => 'iso-8859-1',
+                'wordwrap' => TRUE
+                );
 
-
+            
             $pseudo_contact = htmlspecialchars($_POST['pseudo_contact']);
             $mail_contact = htmlspecialchars($_POST['mail_contact']);
             $titre_contact = htmlspecialchars($_POST['titre_contact']);
             $texte_contact = htmlspecialchars($_POST['texte_contact']);
-    
-            try
-            {
-                // Initilisation PDO
-                $bdd = new PDO('mysql:host=localhost;dbname=test3', 'root', '');
-            }
- 
-            catch (Exception $e)
-            {
-                // En cas d'erreur : message puis exit
-                die('Erreur : ' . $e->getMessage());
-            }
-    
             
-            //Requete SQL
-            $req = $PDO->prepare("INSERT INTO `contact` (`pseudo_contact`, `mail_contact`, `titre_contact`, `texte_contact`) VALUES (:pseudo_contact, :mail_contact, :titre_contact, :texte_contact)");
-            $req->execute(array(
-            ':pseudo_contact' => $pseudo_contact, 
-            ':mail_contact' => $mail_contact,
-            ':titre_contact' => $titre_contact,
-            ':texte_contact' => $texte_contact
-            ));
-            
-            echo($pseudo_contact);
-  
+            $this->load->library('email', $config);
+            $this->email->set_newline("\r\n");
+            $this->email->set_newline("\r\n"); 
+            $this->email->to('finlandblogproject@gmail.com');
+            $this->email->subject($mail_contact);
+            $this->email->message($texte_contact);
+
+            if($this->email->send())
+            {
+                return(TRUE);
+            }
+            else
+            {
+                //show_error($this->email->print_debugger());
+                redirect('accueil/message', 'refresh'); 
+            }
+      
         }
 }
